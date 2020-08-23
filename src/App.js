194428648434d2ac709df883d79315406aee1c37
romulogmp <romulogmp@gmail.com>
2020-08-23 10:10:43 -0300

    Submitting challenge for review

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+        setProjects(response.data);
+    });
+}, []);
+
   async function handleAddRepository() {
-    // TODO
+    const data = {};
+    const response = await api.post('/repositories', data);
+    const repository = response.data;
+    setProjects([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+    const filterRepositories = repositories.filter(rep => rep.id !== id)
+    setProjects(filterRepositories)
   }
-
+  
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(rep => (
+          <li key={rep.id}> {rep.title} 
+            <button onClick={() => handleRemoveRepository(rep.id)}>
+              Remover
+            </button>
+        </li>))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
